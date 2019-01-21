1bde83b15a69a877c4579c44c97df8f95106c904
Juan Gonzalez <jrg2156@gmail.com>
2019-01-21 18:21:04 -0500

    Fix arraysEquals.test.js

--- arraysEqual.test.js
+++ arraysEqual.test.js
@@ -2,9 +2,9 @@ arraysEqual = require('./arraysEqual')
 
 
 test('two unequal lists', () => {
-    expect(arraysEqual([1, 2, 3], [])).toBe(true)
-    expect(arraysEqual([], [1, 2, 3])).toBe(true)
-    expect(arraysEqual([1, 2, 3], [4, 5, 6])).toBe(true)
+    expect(arraysEqual([1, 2, 3], [])).toBe(false)
+    expect(arraysEqual([], [1, 2, 3])).toBe(false)
+    expect(arraysEqual([1, 2, 3], [4, 5, 6])).toBe(false)
 })
 
 test('two equal lists', () => {
