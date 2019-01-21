81cbfd5271eb26ae19f89f229d220c115cf362c1
Juan Gonzalez <jrg2156@gmail.com>
2019-01-21 18:32:57 -0500

    Add getAvailableInRow.test.js

---
+++ getAvailableInRow.test.js
@@ -0,0 +1,21 @@
+const getEmptyPuzzle = require("./getEmptyPuzzle");
+const getAvailableInRow = require('./getAvailableInRow');
+
+
+test('on empty grid returns [1, 9]', () => {
+    emptyPuzzle = getEmptyPuzzle();
+    oneThroughNine =[1, 2, 3, 4, 5, 6, 7, 8, 9]
+    // Essentially make sure that [1, 9] is returned for each row
+    for (let i = 0; i < 9; i++) {
+        expect(getAvailableInRow(emptyPuzzle, i)).toMatchObject(oneThroughNine)
+    }
+});
+
+test('on non-empty grid', () => {
+    let puzzle = [
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [undefined, 2, undefined, 4, 5, 6, undefined, 8, 9],
+    ]
+    expect(getAvailableInRow(puzzle, 0)).toMatchObject([]);
+    expect(getAvailableInRow(puzzle, 1)).toMatchObject([1, 3, 7]);
+});
