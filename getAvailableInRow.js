211ecebe0005db9aa1fe4dc5336f936e70e5c141
Simone Kovacs <dsimonekovacs@gmail.com>
2019-01-21 18:02:59 -0500

    Add getAvailableInRow function

--- getAvailableInRow.js
+++ getAvailableInRow.js
@@ -1,5 +1,10 @@
 function getAvailableInRow(puzzle, rowIndex) {
+    let available = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]);
     
-
-
+    for (let i = 0; i < 9; i++) {
+        available.delete(puzzle[rowIndex][i]);
+    }
+    return Array.from(available);
 }
+
+// checking available numbers against the row
