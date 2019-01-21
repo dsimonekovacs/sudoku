b01bd292d849c06b1c85a583dde1b4738ba2814b
Simone Kovacs <dsimonekovacs@gmail.com>
2019-01-21 16:25:41 -0500

    set up generatePuzzle.js

--- generatePuzzle.js
+++ generatePuzzle.js
@@ -1,6 +1,14 @@
 function generatePuzzle() {
-    console.log('coming soon...')
-
+    let puzzle = getEmptyPuzzle(); // emptyPuzzle returns a 9x9 grid of nulls
+    
+    for (let i = 0; i < 9; i++) {
+        for (j = 0; j < 9; j++) {
+            let available = getAvailable(puzzle, i, j); //return array of available nums for cell
+            let randomNum = chooseRandomly(available);
+            setAt(puzzle, i, j, randomNum)
+        }
+    }
+    return puzzle;
 }
 
 module.exports = generatePuzzle
