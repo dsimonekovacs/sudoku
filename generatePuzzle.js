d63797265b62bb9299c9a5479a4e8982469b5792
Juan Gonzalez <jrg2156@gmail.com>
2019-01-21 16:42:40 -0500

    Use let in j

--- generatePuzzle.js
+++ generatePuzzle.js
@@ -5,7 +5,7 @@ getEmptyPuzzle = require('./getEmptyPuzzle');
 function generatePuzzle() {
     let puzzle = getEmptyPuzzle(); // emptyPuzzle returns a 9x9 grid of nulls
     for (let i = 0; i < 9; i++) {
-        for (j = 0; j < 9; j++) {
+        for (let j = 0; j < 9; j++) {
             let available = getAvailable(puzzle, i, j); //return array of available nums for cell
             let randomNum = chooseRandomly(available);
             setAt(puzzle, i, j, randomNum)
