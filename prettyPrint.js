e99036a0ff12e9480e3622e2ba473c045b767d59
Juan Gonzalez <jrg2156@gmail.com>
2019-01-21 20:17:24 -0500

    Tweak

--- prettyPrint.js
+++ prettyPrint.js
@@ -5,11 +5,11 @@ function prettyPrint(grid) {
             num = num === undefined ? '\033[2;37;31mX\033[0m': num
             process.stdout.write(` ${num} `)
             if (j === 2 || j === 5) {
-                process.stdout.write('|')
+                process.stdout.write('│')
             }
         }
         if (i === 2 || i === 5) {
-            console.log('\n----------------------------')
+            console.log('\n─────────┼─────────┼─────────')
         }
         else {
             console.log('')
